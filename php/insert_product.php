2dfce8c12c72030b5bec2d44f26d0aceeeb04636
Fuumaraed <129939405+fuumaraedd@users.noreply.github.com>
2023-06-12 23:35:21 +0800

    Minor Updates

---
+++ php/insert_product.php
@@ -0,0 +1,50 @@
+<?php
+if (!isset($_POST)) {
+    $response = array('status' => 'failed', 'data' => null);
+    sendJsonResponse($response);
+    die();
+}
+
+include_once("dbconnect.php");
+
+$userid = $_POST['userid'];
+$product_name = $_POST['productname'];
+$product_desc = $_POST['productdesc'];
+$product_price = $_POST['productprice'];
+$product_qty = $_POST['productqty'];
+$product_type = $_POST['type'];
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+$state = $_POST['state'];
+$locality = $_POST['locality'];
+$image1 = $_POST['image1'];
+$image2 = $_POST['image2'];
+$image3 = $_POST['image3'];
+
+$sqlinsert = "INSERT INTO `tbl_products`(`user_id`,`product_name`, `product_desc`, `product_type`, `product_price`, `product_qty`, `product_lat`, `product_long`, `product_state`, `product_locality`) VALUES ('$userid','$product_name','$product_desc','$product_type','$product_price','$product_qty','$latitude','$longitude','$state','$locality')";
+
+if ($conn->query($sqlinsert) === TRUE) {
+	$filename = mysqli_insert_id($conn);
+	$response = array('status' => 'success', 'data' => null);
+	$decoded_string1 = base64_decode($image1);
+	$decoded_string2 = base64_decode($image2);
+	$decoded_string3 = base64_decode($image3);
+	$path1 = '../assets/products/'.$filename.'a.png';
+	$path2 = '../assets/products/'.$filename.'b.png';
+	$path3 = '../assets/products/'.$filename.'c.png';
+	file_put_contents($path1, $decoded_string1);
+	file_put_contents($path2, $decoded_string2);
+	file_put_contents($path3, $decoded_string3);
+    sendJsonResponse($response);
+}else{
+	$response = array('status' => 'failed', 'data' => null);
+	sendJsonResponse($response);
+}
+
+function sendJsonResponse($sentArray)
+{
+    header('Content-Type: application/json');
+    echo json_encode($sentArray);
+}
+
+?>
